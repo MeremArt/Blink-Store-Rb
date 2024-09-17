c79c24d0833dd9e99d3c3054e6930efd31052e34
Ugo Chinemerem <ugofranklin22@gmail.com>
2024-09-17 12:14:26 +0100

    add:tiplink

--- app/components/AppWalletProvider.tsx
+++ app/components/AppWalletProvider.tsx
@@ -4,6 +4,7 @@ import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
+import { useSearchParams } from "next/navigation";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
@@ -13,6 +14,8 @@ import {
 } from "@solana/wallet-adapter-wallets";
 import { BackpackWalletAdapter } from "@solana/wallet-adapter-backpack";
 
+import { TipLinkWalletAdapter } from "@tiplink/wallet-adapter";
+import { TipLinkWalletAutoConnectV2 } from "@tiplink/wallet-adapter-react-ui";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 export default function AppWalletProvider({
@@ -27,14 +30,21 @@ export default function AppWalletProvider({
       new PhantomWalletAdapter(),
       new SolflareWalletAdapter(),
       new BackpackWalletAdapter(),
+      new TipLinkWalletAdapter({
+        title: "Name of Dapp", // Replace with your Dapp's name
+        clientId: "694bf97c-d2ac-4dfc-a786-a001812658df", // Replace with your actual client ID
+        theme: "dark", // Choose between "dark", "light", or "system"
+      }),
     ],
-    []
+    [network]
   );
-
+  const searchParams = useSearchParams();
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets}>
-        <WalletModalProvider>{children}</WalletModalProvider>
+        <TipLinkWalletAutoConnectV2 isReady query={searchParams}>
+          <WalletModalProvider>{children}</WalletModalProvider>
+        </TipLinkWalletAutoConnectV2>
       </WalletProvider>
     </ConnectionProvider>
   );
