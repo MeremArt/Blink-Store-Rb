a3b107da2033c06c8a4d8208003a23b8d15f1179
Ugo Chinemerem <ugofranklin22@gmail.com>
2024-09-18 01:06:22 +0100

    ribh:store

--- app/components/AppWalletProvider.tsx
+++ app/components/AppWalletProvider.tsx
@@ -32,7 +32,7 @@ export default function AppWalletProvider({
       new SolflareWalletAdapter(),
       new BackpackWalletAdapter(),
       new TipLinkWalletAdapter({
-        title: "Name of Dapp", // Replace with your Dapp's name
+        title: "Ribh Store", // Replace with your Dapp's name
         clientId: "65783e67-8b6d-4da9-b2ba-40139abc7557", // Replace with your actual client ID
         theme: "dark", // Choose between "dark", "light", or "system"
       }),
