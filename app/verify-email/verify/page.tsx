d9e932114ee344e3d086fe176a804c3bbd10f95d
Ugo Chinemerem <ugofranklin22@gmail.com>
2024-09-22 16:31:15 +0100

    wallet:changes

--- app/verify-email/verify/page.tsx
+++ app/verify-email/verify/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Image from "next/image";
 import React, { useState } from "react";
 import logo from "@/assets/images/Logo.svg";
@@ -11,38 +11,34 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 function Page() {
-    const router = useRouter();
-    const [isloading , setIsLoading] = useState<boolean>(false);
-    const [userEmail , setUserEmail] = useState<string>('')
-    const [isUserOnWaitlist , setIsUserOnWaitlist] = useState<boolean>(false);
+  const router = useRouter();
+  const [isloading, setIsLoading] = useState<boolean>(false);
+  const [userEmail, setUserEmail] = useState<string>("");
+  const [isUserOnWaitlist, setIsUserOnWaitlist] = useState<boolean>(false);
 
-    const verifyEmail = async (e: any) => {
-      e.preventDefault();
-      setIsLoading(true); 
-  
-      if (!userEmail) {
-          toast.error('Email cannot be empty');
-          setIsLoading(false); 
-          return;
-      }
-  
-      try {
-        const response = await axios.get(
-         `https://www.ribh.xyz/api/v1/user?email=${userEmail}`
+  const verifyEmail = async (e: any) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    if (!userEmail) {
+      toast.error("Email cannot be empty");
+      setIsLoading(false);
+      return;
+    }
+
+    try {
+      const response = await axios.get(
+        `https://www.ribh.xyz/api/v1/user?email=${userEmail}`
       );
-  
-      
-    
-      
-     
+
       const data = response?.data?.data;
       console.log(data, "response");
-  
+
       if (data) {
-        console.log('yes')
-        const { email } = data; 
+        console.log("yes");
+        const { email } = data;
         localStorage.setItem("email", JSON.stringify(email));
-        toast.success("Email verified", {
+        toast.success("Welcome ${email}", {
           position: "top-right",
           autoClose: 5000,
           hideProgressBar: false,
@@ -55,45 +51,42 @@ function Page() {
       } else {
         throw new Error("No data found for this email");
       }
-      } catch (err:any) {
-          console.log(err)
-          if(err.response.data){
-            const getMessage = err.response.data;
-            const {message} = getMessage;
-            toast.error(message, {
-              position: "top-right",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            });
-            router.push('/verify-email/login');
-          };
-
-          toast.error(err?.message, {
-            position: "top-right",
+    } catch (err: any) {
+      console.log(err);
+      if (err.response.data) {
+        const getMessage = err.response.data;
+        const { message } = getMessage;
+        toast.error(message, {
+          position: "top-right",
           autoClose: 5000,
           hideProgressBar: false,
           closeOnClick: true,
           pauseOnHover: true,
           draggable: true,
           progress: undefined,
-          });
-          
-      } finally {
-          setIsLoading(false); 
+        });
+        router.push("/verify-email/login");
       }
+
+      toast.error(err?.message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    } finally {
+      setIsLoading(false);
+    }
   };
-  
 
   return (
-
     <div className="flex flex-col items-center gap-12 flex-[1_0_0] self-stretch max-w-[42rem] h-full">
       <div className="flex flex-col items-end gap-1 self-stretch ">
         {/* <Button label='Back to Home' leftIcon={<ArrowLeft/>} fit size="small" customClassName=" text-[#7839EE] font-Inter text-base mxs:text-[0.875rem] font-semi-bold leading-normal rounded-[2rem] border border-[#7839EE] mmd:bg-[#7839EE] mmd:text-white "/> */}
-        </div>
+      </div>
 
       <div className="flex flex-col items-start gap-14 mxs:gap-10  p-[72px_54px_104px_54px] mxs:p-[32px_24px_12px_24px] self-stretch rounded-lg bg-white shadow-[0px_0px_32px_0px_rgba(0,0,0,0.08)]">
         <div className="flex flex-col items-center gap-10 mxs:gap-[20px] self-stretch">
@@ -109,29 +102,34 @@ function Page() {
             </Typography>
           </div>
         </div>
-        <form className="flex flex-col items-start gap-16 mxs:gap-10 self-stretch" onSubmit={verifyEmail}>
-            <div className="w-full ">
-                <Input name="Email address" label='Email address' placeholder="new.user@mail.com" passwordWay={false} onChange={(e:any)=>setUserEmail(e.target.value)}/>
-            </div>
-                <div className="w-full">
-                    <Button
-                     label='Verify Email' 
-                     type="submit" 
-                     loading={isloading}
-                      size="small" 
-                      customClassName="bg-[#7839EE] rounded-[2rem] mxs:text-[0.875rem] text-white"
-                      />
-                </div>
-            </form>
+        <form
+          className="flex flex-col items-start gap-16 mxs:gap-10 self-stretch"
+          onSubmit={verifyEmail}
+        >
+          <div className="w-full ">
+            <Input
+              name="Email address"
+              label="Email address"
+              placeholder="new.user@mail.com"
+              passwordWay={false}
+              onChange={(e: any) => setUserEmail(e.target.value)}
+            />
+          </div>
+          <div className="w-full">
+            <Button
+              label="Verify Email"
+              type="submit"
+              loading={isloading}
+              size="small"
+              customClassName="bg-[#7839EE] rounded-[2rem] mxs:text-[0.875rem] text-white"
+            />
+          </div>
+        </form>
       </div>
 
       <div className="flex items-end gap-4 flex-[1_0_0] self-stretch">
-        <div className="rounded-[100px] bg-[linear-gradient(90deg,#7839EE_0%,#A27DFF_50.8%,#4E55FF_100%)] h-[12px] flex-[1_0_0]">
-
-        </div>
-        <div className="rounded-[100px] bg-[#EDEDED] h-[12px] flex-[1_0_0]">
-
-        </div>
+        <div className="rounded-[100px] bg-[linear-gradient(90deg,#7839EE_0%,#A27DFF_50.8%,#4E55FF_100%)] h-[12px] flex-[1_0_0]"></div>
+        <div className="rounded-[100px] bg-[#EDEDED] h-[12px] flex-[1_0_0]"></div>
       </div>
     </div>
   );
--- components/second-navbar/index.tsx
+++ components/second-navbar/index.tsx
@@ -1,38 +1,51 @@
-'use client'
-import React from 'react'
-import Image from 'next/image'
-import logo from '@/assets/images/Logo - Ribh Finance.png';
-import { Button } from '../button';
-import Plus from '@/assets/svg-comps/plus';
-import { Typography } from '../typography';
-import avatar from '@/assets/images/Avatar.svg'
-import { useRouter } from 'next/navigation';
+"use client";
+import React from "react";
+import Image from "next/image";
+import logo from "@/assets/images/Logo - Ribh Finance.png";
+import { Button } from "../button";
+import Plus from "@/assets/svg-comps/plus";
+import { Typography } from "../typography";
+import { useWallet, useConnection } from "@solana/wallet-adapter-react";
+import avatar from "@/assets/images/Avatar.svg";
+import Logo from "@/assets/images/Logo.svg";
+import { useRouter } from "next/navigation";
 export default function SecondNavBar() {
-    const router = useRouter();
-    const toHome=()=>{
-        router.push('/verify-email/verify')
+  const router = useRouter();
+  const toHome = () => {
+    router.push("/verify-email/verify");
+  };
+  const shortenString = (str: string | undefined) => {
+    if (str && str.length > 5) {
+      return str.slice(0, 5) + "...";
     }
+    return str; // Return the string as is if it's shorter than 5 characters
+  };
+
+  const { publicKey, signTransaction } = useWallet();
   return (
-    <div className='flex h-[88px] px-[96px] mxs:p-[1rem] w-full py-4 justify-between items-center'>
-        <div onClick={toHome}>
-                <Image src={logo} alt='logo' width={200} height={43.373}/>
-            </div>
-        <div className='flex items-center gap-[32px]'>
-            <div className='flex items-center gap-[16px]'>
-                <Typography customClassName='mmd:hidden'>James Dancer</Typography>
-                <div className=' relative flex w-[48px] h-[48px] justify-center items-center'>
-                    <Image src={avatar} alt='avatar' fill/>
-                </div>
-            </div>
-            <div className='mmd:hidden'>
-                <Button
-                 label='Create Product'
-                  leftIcon={<Plus/>}
-                   customClassName='flex h-[56px] p-[16px_24px] justify-center items-center gap-[8px] bg-[#7839EE] rounded-[32px] text-white'
-                   onClick={()=>router.push('/products/create-products')}
-                   />
-            </div>
+    <div className="flex h-[88px] px-[96px] mxs:p-[1rem] w-full py-4 justify-between items-center">
+      <div onClick={toHome}>
+        <Image src={logo} alt="logo" width={200} height={43.373} />
+      </div>
+      <div className="flex items-center gap-[32px]">
+        <div className="flex items-center gap-[16px]">
+          <Typography customClassName="mmd:hidden">
+            {" "}
+            {shortenString(publicKey?.toString())}
+          </Typography>
+          <div className=" relative flex w-[48px] h-[48px] justify-center items-center">
+            <Image src={avatar} alt="avatar" fill />
+          </div>
         </div>
+        <div className="mmd:hidden">
+          <Button
+            label="Create Product"
+            leftIcon={<Plus />}
+            customClassName="flex h-[56px] p-[16px_24px] justify-center items-center gap-[8px] bg-[#7839EE] rounded-[32px] text-white"
+            onClick={() => router.push("/products/create-products")}
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
